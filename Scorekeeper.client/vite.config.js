f5b62882a3873e056f8c61da105b542e24acdb7a
Brian Crossley <bcrossley712@gmail.com>
2022-07-13 17:11:56 -0600

    production push

--- Scorekeeper.client/vite.config.js
+++ Scorekeeper.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'Scorekeeper/client',
+    outDir: '..Scorekeeper/client',
     sourcemap: false
   },
   server: {
