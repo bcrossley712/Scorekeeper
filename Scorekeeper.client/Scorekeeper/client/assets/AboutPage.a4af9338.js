ac45096901aeef30a755732d7a0a63769dd59e71
Brian Crossley <bcrossley712@gmail.com>
2022-07-13 17:18:09 -0600

    goofed up and trying to fix

--- Scorekeeper.client/Scorekeeper/client/assets/AboutPage.a4af9338.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.a8b9ad1d.js";import{o,b as t,d as s}from"./vendor.bbfce40b.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,b,u,f){return o(),t("div",c,r)}var m=e(a,[["render",_]]);export{m as default};
