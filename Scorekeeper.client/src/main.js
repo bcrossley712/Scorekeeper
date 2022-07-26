04a68786cafe6b7db2d56f5615b018af6f12beeb
Brian Crossley <bcrossley712@gmail.com>
2022-07-26 11:13:46 -0600

    starting to add pwa

--- Scorekeeper.client/src/main.js
+++ Scorekeeper.client/src/main.js
@@ -5,6 +5,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
+import './registerServiceWorker'
 
 const root = createApp(App)
 registerGlobalComponents(root)
