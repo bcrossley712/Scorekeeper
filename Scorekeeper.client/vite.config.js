f1e65d04ec4a7cc5bfd036888c1469de78d9712f
Brian Crossley <bcrossley712@gmail.com>
2022-07-27 09:48:22 -0600

    production push

--- Scorekeeper.client/vite.config.js
+++ Scorekeeper.client/vite.config.js
@@ -4,7 +4,34 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    includeAssets: ['/public/img/icons/favicon-32x32.png', '/public/img/apple-touch-icon.png', '/public/img/safari-pinned-tab.svg'],
+    manifest: {
+      name: 'Scorekeeper',
+      short_name: 'Scorekeeper',
+      description: 'Card game scorekeeper',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/public/img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: '/public/img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '/public/img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }
+  })],
   build: {
     outDir: '../Scorekeeper/client',
     sourcemap: false
